11caa50cc1bf228214a2ad04e0348b7dceb35be1
W1NDes <w1ndes@outlook.com>
2025-07-13 05:34:05 +0800

    Upd(ShipIr): 添加彩船识别，优化识别流程

--- module/ship_ir/ship_ir.py
+++ module/ship_ir/ship_ir.py
@@ -1,5 +1,4 @@
 import random
-
 from module.base.button import Button, ButtonGrid
 from module.ocr.ocr import Ocr
 from module.logger import logger
@@ -8,17 +7,14 @@ from module.ship_ir.assets import *
 from module.ship_ir.utils import convert_filter_to_params
 from module.ship_ir.handbook import Handbook
 
-NAMEBAR_G = [NAMEBAR1_G, NAMEBAR2_G, NAMEBAR3_G, NAMEBAR4_G, NAMEBAR5_G, NAMEBAR6_G]
-NAMEBAR_P = [NAMEBAR1_P, NAMEBAR2_P, NAMEBAR3_P, NAMEBAR4_P, NAMEBAR5_P, NAMEBAR6_P]
-NAMEBAR_LIST = [NAMEBAR1_G, NAMEBAR1_P, NAMEBAR2_G, NAMEBAR2_P, NAMEBAR3_G, NAMEBAR3_P, NAMEBAR4_G, NAMEBAR4_P, NAMEBAR5_G, NAMEBAR5_P, NAMEBAR6_G, NAMEBAR6_P]
 HANDBOOK_SCROLL = Scroll(HANDBOOK_SCROLL_AREA, color=(244, 208, 66))
 SHIP_NAME_BUTTON = ButtonGrid(origin=(200, 448), delta=(164 + 1 / 3, 226), button_shape=(135,21), grid_shape=(6, 2), name='SHIP_NAME')
 
-CHECK_FILTER1 = ["前排先锋","白鹰","稀有","无限制"]
-CHECK_FILTER2 = ["轻巡","白鹰","精锐","无限制"]
-CHECK_FILTER3 = ["轻巡","重樱","全部","无限制"]
-CHECK_FILTER4 = ["战列","白鹰","金色","无限制"]
-CHECK_FILTER5 = ["航母","皇家","全部","无限制"]
+CHECK_FILTER1 = ["全部","全阵营","彩色","无限制"]
+CHECK_FILTER2 = ["","白鹰","精锐","无限制"]
+CHECK_FILTER3 = ["","重樱","全部","无限制"]
+CHECK_FILTER4 = ["","白鹰","金色","无限制"]
+CHECK_FILTER5 = ["","皇家","全部","无限制"]
 CHECK_FILTER_LIST = [CHECK_FILTER1, CHECK_FILTER2, CHECK_FILTER3, CHECK_FILTER4, CHECK_FILTER5]
 
 class ShipIr(Handbook):
@@ -44,11 +40,11 @@ class ShipIr(Handbook):
             filtered_result = ''
         return filtered_result
     
-    def ship_ir_by_namebar(self,recognized_names):
+    def ship_ir_by_namebar(self,recognized_names,namebar_list):
         """
         通过边框位置识别角色名
         """
-        for name_bar in NAMEBAR_LIST:
+        for name_bar in namebar_list:
             if self.appear(name_bar,offset=(30,70),similarity=0.75):
                 logger.info(name_bar.button[1])
                 name = Button(area=(name_bar.button[0]+1, name_bar.button[1]-30, name_bar.button[2]-1, name_bar.button[3]-19), color=(),button=(0, 1, 0, 2))
@@ -71,25 +67,49 @@ class ShipIr(Handbook):
                 logger.info(f'识别为：{ship_name}')
         return recognized_names
     
-    def ship_ir(self,recognized_names,skip_first_screenshot=True):
+    def summon_namebar_list(self,rarity):
+        # 'all', 'common', 'rare', 'elite', 'super_rare', 'ultra'
+        namebars = {
+            1: {'G': NAMEBAR1_G, 'P': NAMEBAR1_P, 'U': NAMEBAR1_U},
+            2: {'G': NAMEBAR2_G, 'P': NAMEBAR2_P, 'U': NAMEBAR2_U},
+            3: {'G': NAMEBAR3_G, 'P': NAMEBAR3_P, 'U': NAMEBAR3_U},
+            4: {'G': NAMEBAR4_G, 'P': NAMEBAR4_P, 'U': NAMEBAR4_U},
+            5: {'G': NAMEBAR5_G, 'P': NAMEBAR5_P, 'U': NAMEBAR5_U},
+            6: {'G': NAMEBAR6_G, 'P': NAMEBAR6_P, 'U': NAMEBAR6_U},
+        }
+        if rarity == 'all':
+            return [namebars[i][s] for i in range(1, 7) for s in ['G', 'P', 'U']]
+        elif any(rarity == i for i in ['common', 'rare']):
+            return [namebars[i][s] for i in range(1, 7) for s in ['G', 'P']]
+        elif rarity == 'elite':
+            return [namebars[i]['P'] for i in range(1, 7)]
+        elif rarity == 'super_rare':
+            return [namebars[i]['G'] for i in range(1, 7)]
+        elif rarity == 'ultra':
+            return [namebars[i]['U'] for i in range(1, 7)]
+        else:
+            return [namebars[i][s] for i in range(1, 7) for s in ['G', 'P']]
+        
+    def ship_ir(self,recognized_names,rarity,skip_first_screenshot=True):
         # while len(recognized_names) < 36:
         # HANDBOOK_SCROLL.set_top(main=self)
+        namebar_list = self.summon_namebar_list(rarity)
         while 1:
             if skip_first_screenshot:
                 skip_first_screenshot = False
             else:
                 self.device.screenshot()
 
-            if HANDBOOK_SCROLL.cal_position(main=self) >= 0.989 or abs(HANDBOOK_SCROLL.length-HANDBOOK_SCROLL.total) <= 0.05:
+            if HANDBOOK_SCROLL.cal_position(main=self) >= 0.96 or abs(HANDBOOK_SCROLL.length-HANDBOOK_SCROLL.total) <= 0.05:
                 # HANDBOOK_SCROLL.drag_page(page=0.7, random_range=(0.0, 0.0), main=self,control_check=False)
                 for _ in range(5):
                     self.handbook_swipe(random.randint(400,900))
                 logger.info('Handbook reach bottom, stop')
-                recognized_names = self.ship_ir_by_namebar(recognized_names)
+                recognized_names = self.ship_ir_by_namebar(recognized_names,namebar_list)
                 recognized_names = self.ship_ir_by_area(recognized_names)
                 break
             else:
-                recognized_names = self.ship_ir_by_namebar(recognized_names)
+                recognized_names = self.ship_ir_by_namebar(recognized_names,namebar_list)
                 HANDBOOK_SCROLL.drag_page(page=0.07, random_range=(0.0, 0.0), main=self,control_check=False)
                 # self.handbook_swipe(random.randint(400,900))
                 continue
@@ -116,14 +136,14 @@ class ShipIr(Handbook):
                 continue
             params = convert_filter_to_params(check_filter)
             self.dock_filter_set(**params)
-            recognized_names=self.ship_ir([])
+            recognized_names=self.ship_ir([],params['rarity'])
             logger.info(f"过滤器{i+1}识别到{len(recognized_names)}个角色: {recognized_names}")
             recognized_names_str = ",".join(recognized_names)
             
             check_filter[3] = "未获取"
             params = convert_filter_to_params(check_filter)
             self.dock_filter_set(**params)
-            recognized_names_unget=self.ship_ir([])
+            recognized_names_unget=self.ship_ir([],params['rarity'])
             logger.info(f"过滤器{i+1}识别到未获取{len(recognized_names_unget)}个角色: {recognized_names_unget}")
             recognized_names_unget_str = ",".join(recognized_names_unget)
 
@@ -133,7 +153,9 @@ class ShipIr(Handbook):
 
 if __name__ == '__main__':
     self = ShipIr('alas')
-    CHECK_FILTER1 = ["前排先锋","白鹰","稀有","无限制"]
+    CHECK_FILTER1 = ["前排先锋","白鹰","金色","无限制"]
+    params = convert_filter_to_params(CHECK_FILTER1)
+    print(self.summon_namebar_list(params['rarity']))
     CHECK_FILTER2 = ["轻巡","白鹰","精锐","无限制"]
     CHECK_FILTER3 = ["轻巡","重樱","全部","无限制"]
     CHECK_FILTER4 = ["战列","白鹰","金色","无限制"]
@@ -145,13 +167,13 @@ if __name__ == '__main__':
     for check_filter in CHECK_FILTER_LIST:
         params = convert_filter_to_params(check_filter)
         self.dock_filter_set(**params)
-        recognized_names=self.ship_ir([])
+        recognized_names=self.ship_ir([],params['rarity'])
         logger.info(f"识别到{len(recognized_names)}个角色: {recognized_names}")
 
         check_filter[3] = "未获取"
         params = convert_filter_to_params(check_filter)
         self.dock_filter_set(**params)
-        recognized_names=self.ship_ir([])
+        recognized_names=self.ship_ir([],params['rarity'])
         logger.info(f"识别到{len(recognized_names)}个角色: {recognized_names}")
         break
 
