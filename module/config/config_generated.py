e499d846d437a7b3cd1074c6e5e026ea6b0c98c8
W1NDes <w1ndes@outlook.com>
2025-07-15 00:09:35 +0800

    Feat(7Dtask): 增加7日任务的通用领取

--- module/config/config_generated.py
+++ module/config/config_generated.py
@@ -429,6 +429,7 @@ class GeneratedConfig:
 
     # Group `Smallevent`
     Smallevent_SevenDayTask = True
+    Smallevent_UpdateInfoImmediately = False
 
     # Group `Oilkeep`
     Oilkeep_OilkeepLevel = 8000
--- module/smallevent/samllevent.py
+++ module/smallevent/samllevent.py
@@ -1,3 +1,8 @@
+import sys
+
+import re
+sys.path.append(r'C:/Users/W1NDe/Documents/GitHub/M-AzurLaneAutoScript')
+from module.base.button import ButtonGrid
 from module.ui.ui import UI
 from module.ui.page import page_main
 from module.smallevent.assets import * 
@@ -6,7 +11,8 @@ from module.combat.assets import GET_ITEMS_1
 from module.logger import logger
 from module.base.timer import Timer
 from module.log_res.log_res import LogRes
-
+from module.base.utils import crop
+from module.ocr.api_ocr import BaiduOcr
 class SmallEvent(UI):
     SEVEND_DATE = "20250320"  # 集中管理活动日期
     
@@ -122,23 +128,220 @@ class SmallEvent(UI):
                 
             
     def run(self):
-        # LogRes(self.config).SevenDayStatus += 11
-        # logger.hr(LogRes(self.config).SevenDayStatus)
-        # logger.hr(self.config.cross_get('Dashboard.SevenDayStatus.Value'))
-        if self.config.Smallevent_SevenDayTask == True:
-            task_icon = f"SEVEND_TASK_{self.SEVEND_DATE}"
-            task_get1 = f"SEVEND_TASK_GET1_{self.SEVEND_DATE}"
-            task_get2 = f"SEVEND_TASK_GET2_{self.SEVEND_DATE}"
-            task_finish = f"SEVEND_TASK_FINISH_{self.SEVEND_DATE}"
-            # task_unget1 = f"SEVEND_TASK_UNGET1_{self.SEVEND_DATE}"
-            # task_unget2 = f"SEVEND_TASK_UNGET2_{self.SEVEND_DATE}"   
-            self.SevenDayTask(
-                SEVEND_TASK_ICON_MAIN=globals()[task_icon],
-                SEVEND_TASK_GET1=globals()[task_get1],
-                SEVEND_TASK_GET2=globals()[task_get2],
-                SEVEND_TASK_FINISH=globals()[task_finish],
-                # SEVEND_TASK_UNGET1=globals()[task_unget1],
-                # SEVEND_TASK_UNGET2=globals()[task_unget2]
-            )
-        else:LogRes(self.config).SevenDayStatus = 0
+        # # LogRes(self.config).SevenDayStatus += 11
+        # # logger.hr(LogRes(self.config).SevenDayStatus)
+        # # logger.hr(self.config.cross_get('Dashboard.SevenDayStatus.Value'))
+        # if self.config.Smallevent_SevenDayTask == True:
+        #     task_icon = f"SEVEND_TASK_{self.SEVEND_DATE}"
+        #     task_get1 = f"SEVEND_TASK_GET1_{self.SEVEND_DATE}"
+        #     task_get2 = f"SEVEND_TASK_GET2_{self.SEVEND_DATE}"
+        #     task_finish = f"SEVEND_TASK_FINISH_{self.SEVEND_DATE}"
+        #     # task_unget1 = f"SEVEND_TASK_UNGET1_{self.SEVEND_DATE}"
+        #     # task_unget2 = f"SEVEND_TASK_UNGET2_{self.SEVEND_DATE}"   
+        #     self.SevenDayTask(
+        #         SEVEND_TASK_ICON_MAIN=globals()[task_icon],
+        #         SEVEND_TASK_GET1=globals()[task_get1],
+        #         SEVEND_TASK_GET2=globals()[task_get2],
+        #         SEVEND_TASK_FINISH=globals()[task_finish],
+        #         # SEVEND_TASK_UNGET1=globals()[task_unget1],
+        #         # SEVEND_TASK_UNGET2=globals()[task_unget2]
+        #     )
+        # else:LogRes(self.config).SevenDayStatus = 0
+        page_area = (281, 79, 1254, 560)
+        if self.goto_sevenD_page(page_area):
+            self.get_reward(page_area)
+            if self.config.Smallevent_UpdateInfoImmediately == True:
+                self.device.sleep(1)
+                self.device.screenshot()
+                update_words = self.recognize_activity_page(self.device.image,page_area)
+                if update_words:
+                    self.recognize_activiy_status(update_words)
+            pass
+        else:
+            logger.warning("未成功进入七天小任务页面")
+            pass
         self.config.task_delay(server_update=True)
+
+    def recognize_text(self, image, area,model="general_basic"):
+        """
+        Use Baidu OCR API to recognize the text
+        
+        Args:
+            image: Original image
+            area: Crop area (x1, y1, x2, y2)
+            model: Recognition model, can be "general_basic" or "accurate_basic"
+        """
+        original_crop = crop(image, area)
+        
+        result = BaiduOcr(self.config).request_baidu_ocr(original_crop,area,model)
+        if result:
+            if 'words_result' in result and len(result['words_result']) > 0:
+                return result
+            else:
+                logger.info(result)
+        return False
+    
+    def recognize_activity_page(self,image,area=(281, 79, 1254, 560)):
+        result = self.recognize_text(image, area)
+        if result:
+            all_words = "".join([word['words'] for word in result['words_result']])
+            if "每日0点" in all_words or "解锁2个任务" in all_words:
+                logger.info(f"发现七天小任务：{all_words}")
+                return all_words
+            logger.info('当前页未发现七天小任务')
+            return False 
+        else:
+            logger.warning('Failed to recognize text')
+            return False
+        
+    def recognize_activiy_status(self,text):
+        if "进度" in text:
+            progress_text = text.split("进度")[1]
+            progress_number = progress_text.split("/")[0]
+            progress_number = re.sub(r'[^0-9]', '', progress_number)#正则匹配掉非数字
+            LogRes(self.config).SevenDayStatus = int(progress_number)
+            logger.info(f"更新七天小任务进度：{progress_number}")
+        #统计text中有几个领取，几个前往
+        
+        go_count = text.count("前往")
+        got_count = text.count("已领取")
+        get_count = text.count("领取") - got_count
+        logger.info(f"发现{go_count}个前往按钮，发现{got_count}个已领取按钮，发现{get_count}个领取按钮")
+        return go_count
+    
+    def locate_button_by_text(self,image,text,text_exclude=None,area=(281, 79, 1254, 560),interval=0):
+        if interval:
+            timer = self.get_interval_timer(text, interval=interval, renew=True)
+            if not timer.reached():
+                return "cooldowning"
+        result = self.recognize_text(image, area,model="general")
+        if result:
+            for word in result['words_result']:
+                if text in word['words'] and not any(text_exclude in word['words'] for text_exclude in text_exclude):
+                    logger.info(f"发现{text}按钮：{word['words']}")
+                    if interval:
+                        self.get_interval_timer(text).reset()
+                    return word['location']
+        return None
+    
+    def location_2_button(self,location,name,base_loc):
+        area = (base_loc[0] + location['left'], base_loc[1] + location['top'], base_loc[0] + location['left'] + location['width'], base_loc[1] + location['top'] + location['height'])
+        logger.info(f"将{name}按钮的区域转换为：{area}")
+        return Button(area=area, color=(), button=area,name=name)
+    
+    def get_reward(self,page_area,skip_first_screenshot=True):
+        while 1:
+            if skip_first_screenshot:
+                skip_first_screenshot = False
+            else:
+                self.device.screenshot()
+
+            location = self.locate_button_by_text(self.device.image, "领取", ["已领取"], page_area,interval=5)
+            if isinstance(location, dict):
+                button = self.location_2_button(location, "领取", base_loc=page_area)
+                self.device.click(button)
+                continue
+            elif location == "cooldowning":
+                pass
+                # logger.warning("领取按钮冷却中")
+            else:
+                logger.warning("领取按钮不存在")
+                return False
+
+            if self.story_skip():
+                continue   
+            if self.appear_then_click(POPUP_CONFIRM,offset=(10, 10), interval=1):
+                continue
+            if self.appear_then_click(GET_ITEMS_1,offset=(10, 10), interval=1):
+                continue 
+
+    def goto_sevenD_page(self,page_area):
+        self.ui_ensure(page_main)
+        skip_first_screenshot = True
+        CLICK_COUNT = 0
+        NOCLICK_COUNT = 0
+        NOCLICK_TIMER =Timer(3,count=10)
+        event_grid = ButtonGrid(origin=(12, 121), delta=(0, 82), button_shape=(150, 29), grid_shape=(1, 3))
+        drag_down_timer = self.get_interval_timer("drag_down", interval=2, renew=True)
+        too_low_drag = False
+        while 1:
+            if skip_first_screenshot:
+                skip_first_screenshot = False
+            else:
+                self.device.screenshot()
+            if self.appear_then_click(EVENT_NOTIFY_ENTRY, offset=(5, 5), interval=3):
+                continue
+            if CLICK_COUNT == 3:
+                if drag_down_timer.reached() or too_low_drag:
+                    if not self.appear(ACTIVITY_BOTTOM,offset=(5,3),similarity=0.95) and \
+                        not self.appear(ACTIVITY_BOTTOM_2,offset=(5,3),similarity=0.95):
+                        logger.info("ACTIVITY_BOTTOM not appear")
+                        self.device.drag((100,510),(105,400))
+                        self.device.sleep(1)
+                        self.get_interval_timer("drag_down").reset()
+                        too_low_drag = False
+                        continue
+                    else:
+                        logger.info("ACTIVITY_BOTTOM appear")
+                        break
+                if self.appear(ACTIVITY_SELECTED_GRID,offset=(10,400),similarity=0.75):
+                    # if ACTIVITY_SELECTED_GRID.button[1] > 350:
+                    #     logger.info("selected button too low")
+                    #     too_low_drag = True
+                    #     continue
+                    event_grid = ButtonGrid(origin=(12,ACTIVITY_SELECTED_GRID.button[1]+27+82), delta=(0, 82), button_shape=(150, 29), grid_shape=(1, 3))
+                    CLICK_COUNT = 0
+                    # logger.warning(
+                    #     f"Selected grid updated, new area: (12, {ACTIVITY_SELECTED_GRID.button[1] + 27 + 82},\
+                    #                                     121, {ACTIVITY_SELECTED_GRID.button[1] + 27 + 82 + 29})"
+                    # )
+                    continue
+            elif CLICK_COUNT < 3:
+                if self.appear(EVENT_NOTIFY_PAGE, offset=(5,5)):
+                    all_words = self.recognize_activity_page(self.device.image,page_area)
+                    # all_words = None
+                    if not all_words:
+                        if event_grid.buttons[CLICK_COUNT].button[3] > 585:
+                            CLICK_COUNT = 3
+                            too_low_drag = True
+                            logger.info("click too low")
+                            continue
+                        self.device.click(event_grid.buttons[CLICK_COUNT],control_check=False)
+                        self.device.sleep(0.3)
+                        logger.info(f"Clicking event_grid count: {CLICK_COUNT}")
+                        CLICK_COUNT += 1
+                        continue
+                    else:
+                        go_count = self.recognize_activiy_status(all_words)
+                        if go_count >= 2:
+                            logger.info("七天小任务当前没有可领取项")
+                            return False
+                        return True
+            NOCLICK_TIMER.start()
+            if NOCLICK_TIMER.reached():
+                NOCLICK_COUNT += 1
+                NOCLICK_TIMER.reset()
+                if NOCLICK_COUNT >= 5:
+                    logger.info("GOTO_SEVEND_TASK No click TIMER REACHED")
+                    break
+        return False
+    
+if __name__ == "__main__":
+    self = SmallEvent('zTTT')
+    # from adbutils import AdbClient
+    # self.adb=AdbClient(host="127.0.0.1", port=16448)
+    self.device.screenshot()
+    page_area = (281, 79, 1254, 560)
+
+    if self.goto_sevenD_page():
+        self.get_reward(page_area)
+        if self.config.Smallevent_SevenDayTask == True:
+            self.device.sleep(1)
+            self.device.screenshot()
+            update_words = self.recognize_activity_page(self.device.image,page_area)
+            if update_words:
+                self.recognize_activiy_status(update_words)
+        pass
+    else:
+        logger.warning("未成功进入七天小任务页面")
+        pass
