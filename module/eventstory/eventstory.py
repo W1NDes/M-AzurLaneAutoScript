6d2b29b3e43ec2f640e2e8f886285a1c11a2232a
W1NDes <w1ndes@outlook.com>
2025-10-24 01:41:41 +0800

    Upd(event): regular update

--- module/eventstory/eventstory.py
+++ module/eventstory/eventstory.py
@@ -250,7 +250,7 @@ class EventStory(CampaignUI, Combat, LoginHandler):
         return 'unknown'
 
     def run(self):
-        if datetime.now() < datetime(2025, 10, 3, 0, 0, 0):#eventSet
+        if datetime.now() < datetime(2025, 11, 6, 12, 0, 0):#eventSet
             self.run_event_story()
         else:
             logger.info('Event story expired')
