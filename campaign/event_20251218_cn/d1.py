3f8edc2da8a49e8e885099f9a923f8a5b4c1e62a
W1NDes <w1ndes@outlook.com>
2025-12-19 12:14:18 +0800

    Upd(event): event config regularly update

--- campaign/event_20251218_cn/d1.py
+++ campaign/event_20251218_cn/d1.py
@@ -79,6 +79,9 @@ class Config:
     MAP_SWIPE_MULTIPLY = (1.232, 1.255)
     MAP_SWIPE_MULTIPLY_MINITOUCH = (1.191, 1.213)
     MAP_SWIPE_MULTIPLY_MAATOUCH = (1.156, 1.177)
+    STAGE_INCREASE_CUSTOM = [
+        'A1 > A2 > A3 > B1 > B2 > B3 > C1 > C2 > C3 > D1 > D2 > D3',
+    ]
 
 
 class Campaign(CampaignBase):
