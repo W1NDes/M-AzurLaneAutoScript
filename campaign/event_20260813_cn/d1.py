ea0869d01a35054715733993b84b919ed567a754
W1NDes <w1ndes@outlook.com>
2026-08-14 18:35:19 +0800

    Upd(event)：更新活动关卡配置

--- campaign/event_20260813_cn/d1.py
+++ campaign/event_20260813_cn/d1.py
@@ -75,6 +75,9 @@ class Config:
     MAP_SWIPE_MULTIPLY = (1.136, 1.158)
     MAP_SWIPE_MULTIPLY_MINITOUCH = (1.099, 1.119)
     MAP_SWIPE_MULTIPLY_MAATOUCH = (1.067, 1.086)
+    STAGE_INCREASE_CUSTOM = [
+        'A1 > A2 > A3 > B1 > B2 > B3 > C1 > C2 > C3 > D1 > D2 > D3',
+    ]
 
 
 class Campaign(CampaignBase):
